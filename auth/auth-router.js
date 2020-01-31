02e39e34fa040a0634bc7e8795c0542cf8bb1cea
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-01-31 10:03:43 -0800

    modified login to account for both users and stylists

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,69 @@ const { validateUser } = require('../users/users-helper.js');
 const { validateStylists } = require('../stylists/stylists-helper.js');
 
 
-//USER
+function getJwtToken(username){
+  const payload = {
+    username,
+    role: 'user' || 'stylist' 
+  };
+  const secret = process.env.JWT_SECRET || 'Beautiful Hair';
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+  let istStylist = req.decodedJwt.isStylist
+
+  if (istStylist === true){
+    Stylist.findBy({ username })
+    .first()
+    .then(stylist => {
+      if (stylist && bcrypt.compareSync(password, stylist.password)) {
+        const token = getJwtToken(stylist.username);
+        req.body.username = user.username;
+
+        res.status(200).json({
+          message: `Welcome back, ${stylist.username}.`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Username or password is incorrect.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  }
+
+  else {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = getJwtToken(user.username);
+          req.body.username = user.username;
+
+          res.status(200).json({
+            message: `Welcome back, ${user.username}.`,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: 'Username or password is incorrect.' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+
+});
+
+
+// REGISTER
 router.post('/register/user', (req, res) => {
     let user = req.body;
     const validateResults = validateUser(user);
@@ -26,33 +88,8 @@ router.post('/register/user', (req, res) => {
     } else {
       res.status(400).json({message:'Error:', err: validateResults.errors})
     }
-  });
-
-router.post('/login/user', (req, res) => {
-  let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = getJwtToken(user.username);
-        req.body.username = user.username;
-
-        res.status(200).json({
-          message: `Welcome back, ${user.username}.`,
-          token,
-        });
-      } else {
-        res.status(401).json({ message: 'Username or password is incorrect.' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
 });
 
-
-//STYLIST
 router.post('/register/stylist', (req, res) => {
   let stylist = req.body;
   const validateResults = validateStylists(stylist);
@@ -73,45 +110,9 @@ router.post('/register/stylist', (req, res) => {
   }
 });
 
-router.post('/login/stylist', (req, res) => {
-  let { username, password } = req.body;
-
-  Stylists.findBy({ username })
-    .first()
-    .then(stylist => {
-      if (user && bcrypt.compareSync(password, stylist.password)) {
-        req.body.username = stylist.username;
-
-        const token = getJwtToken(stylist.username);
-
-        res.status(200).json({
-          message: `Welcome back, ${stylist.username}.`,
-          token,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-
-function getJwtToken(username){
-  const payload = {
-    username,
-    role: 'user' || 'stylist' 
-  };
-  const secret = process.env.JWT_SECRET || 'Beautiful Hair';
-  const options = {
-    expiresIn: '1d'
-  };
-
-  return jwt.sign(payload, secret, options);
-}
-
 module.exports = router;
+
+
 
 // function checkRole(role){
 //   return function(req, res, next){
@@ -121,4 +122,5 @@ module.exports = router;
 //       res.status(403).json({message: 'Access denied'})
 //     }
 //   }
+
 // }
