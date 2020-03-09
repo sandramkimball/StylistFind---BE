43e9a83d42d2a0ed1fd964dfdf58e92498b2443c
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-09 14:47:25 -0700

    added origin options to cors

--- api/server.js
+++ api/server.js
@@ -11,14 +11,18 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use( cors() );
+
+const corsOptions = {
+  origin: '*'
+}
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/stylists', stylistsRouter);
 server.use('/api/search', searchRouter);
 
-server.get('/', cors(), (req, res) => {
+server.get('/', cors(corsOptions), (req, res) => {
   res.send("Let\'s find you a stylist.");
 });
 
