ecc0ac59398310853bb24beca646a61d00715205
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-04-02 20:09:05 -0700

    updated mixed matched seed posts

--- database/seeds/06-posts.js
+++ database/seeds/06-posts.js
@@ -2,32 +2,27 @@
 exports.seed = function(knex) {
       return knex('posts').insert([
         {
-          date: '2018-11-15',
-          image: 'https://images.unsplash.com/photo-1549236177-f9b0031756eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-          stylist_id: 1,
-        },
-        {
           date: '2018-10-13',
-          comment: 'Come visit us here at Stella\'s!',
+          comment: 'Richie\'s Barbershop is waiting for yuh.',
           image: 'https://images.unsplash.com/photo-1550091354-bae8a0afb246?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1833&q=80',
           stylist_id: 1,
         },
-        {
+        { 
           date: '2018-11-15',
-          comment: 'Come visit us here at Stella\'s!',
-          image: 'https://images.unsplash.com/photo-1554519515-242161756769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+          comment: 'Another great day at the shop.',
+          image: 'https://images.unsplash.com/photo-1534297635766-a262cdcb8ee4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
           stylist_id: 1,
         },
         {
           date: '2018-11-16',
-          comment: 'Come visit us here at Stella\'s!',
+          comment: 'You wanna look like a gentleman or a homeless man?',
           image: 'https://images.unsplash.com/photo-1517832606299-7ae9b720a186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
           stylist_id: 1,
         },
         {
-          date: '2018-11-15',
-          image: 'https://images.unsplash.com/photo-1522336284037-91f7da073525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-          stylist_id: 1
+          date: '2019-12-05',
+          image: 'https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+          stylist_id: 1,
         },
         {  
           date: '2018-11-15',
@@ -40,21 +35,25 @@ exports.seed = function(knex) {
           image: 'https://images.unsplash.com/photo-1552425883-1fd7e6a11c31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
           stylist_id: 2,
         },
-        { 
+        {
           date: '2018-11-15',
-          comment: 'Another great day at the salon.',
-          image: 'https://images.unsplash.com/photo-1534297635766-a262cdcb8ee4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+          image: 'https://images.unsplash.com/photo-1522336284037-91f7da073525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
           stylist_id: 3,
         },
-      
+        {
+          date: '2018-11-15',
+          comment: 'Come see us here at Stella\'s!',
+          image: 'https://images.unsplash.com/photo-1554519515-242161756769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+          stylist_id: 3,
+        },
         {
           date: '2019-09-17',
           image: 'https://images.unsplash.com/photo-1522337094846-8a818192de1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
           stylist_id: 3,
         },
         {
-          date: '2019-12-05',
-          image: 'https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+          date: '2018-11-15',
+          image: 'https://images.unsplash.com/photo-1549236177-f9b0031756eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
           stylist_id: 3,
         },
         {
