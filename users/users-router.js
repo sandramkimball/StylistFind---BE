b1dcafc9e9a9aa4420717c3b95ae44fc71a53dab
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-04-28 12:17:26 -0700

    edited user review api--where id matches user, not stylist

--- users/users-router.js
+++ users/users-router.js
@@ -44,7 +44,7 @@ router.get('/:id/reviews', restricted, (req, res) => {
       'stylists.last_name as stylist_last',
       'salons.salon')
     .from('reviews')
-    .where('reviews.stylist_id', '=', `${id}`)
+    .where('reviews.user_id', '=', `${id}`)
     .join('users', 'users.id', '=', 'reviews.user_id')
     .join('stylists', 'stylists.id', '=', 'reviews.stylist_id')
     .join('salons', 'salons.stylist_id', '=', 'revoews.stylist_id')
@@ -76,46 +76,15 @@ router.post('/:id/reviews', restricted, (req, res) => {
   const newReview = req.body
   db('reviews').insert(newReview)
   .then(ids => {
-    res.status(201).json({ message: 'Your review was added.' });
+    res.status(201).json({ message: 'Review successfully added.' });
   })
   .catch(err => {
     res.status(500).json({ message: 'Failed to add new review.' });
   });
 });
 
-const multer = require('multer')
-const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null, file.filename + '-' + Date.now().toISOString())
-  }
-})
-const upload = multer({storage: storage})
-
 
 //PUT
-router.put('/:id/upload', restricted, (req, res) => {
-  const id = req.params.id;
-  upload(req, res, (err) => {
-    if (err){
-      res.render('err', {msg: err})
-    } 
-    else {
-      if (req.file == undefined){
-        res.status(500).json({message: 'No file selected'})
-      } else {
-        db('users').where({id}).update(req.file)
-        .then(ids => {
-          res.status(200).json({
-            msg: 'File uploaded',
-            file: `uploads/${req.file.filename}`
-          })
-        })
-      }
-    }
-  })
-})
-
 router.put('/:id', restricted, (req, res) => {
   const userData = req.body;
   const id = req.params.id;
@@ -126,19 +95,6 @@ router.put('/:id', restricted, (req, res) => {
   })
   .catch(err => {
     res.status(500).json({ message: 'Failed to edit user information.', err });
-  });
-});
-
-router.put('/:id/reviews/:id', restricted, (req, res) => {
-  const editedReview = req.body;
-  const id = req.params.id;
-
-  db('reviews').where({id}).update(editedReview)
-  .then(ids => {
-    res.status(201).json({ message: 'Your review was updated.' });
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to edit review.' });
   });
 });
 
@@ -171,3 +127,33 @@ router.delete('/:id/reviews/:id', restricted, (req, res) => {
 module.exports = router;
 
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null, file.filename + '-' + Date.now().toISOString())
+  }
+})
+const upload = multer({storage: storage})
+// this is a test api for images
+router.put('/:id/upload', multer(), (req, res) => {
+  const id = req.params.id;
+  upload(req, res, (err) => {
+    if (err){
+      res.render('err', {msg: err})
+    } 
+    else {
+      if (req.file == undefined){
+        res.status(500).json({message: 'No file selected'})
+      } else {
+        db('users').where({id}).update(req.file)
+        .then(ids => {
+          res.status(200).json({
+            msg: 'File uploaded',
+            file: `uploads/${req.file.filename}`
+          })
+        })
+      }
+    }
+  })
+})
