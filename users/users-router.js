c7a55a8dcddf181114f5a87205c68f71e73dc504
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-05-19 13:15:26 -0700

    req file func

--- users/users-router.js
+++ users/users-router.js
@@ -143,7 +143,7 @@ function checkFile(file, cb){
   //Allowed ext
   const filetypes = /jpeg|jpg|png/;
   //Check ext
-  const extname = filetypes.test(req.file.path.extname(file.originalname).toLowerCase());
+  const extname = filetypes.test(file.path.extname(file.originalname).toLowerCase());
   //Check mime(application/json or img/jpeg?)
   const mimetype = filetypes.test(file.mimetype);
 
@@ -156,7 +156,7 @@ function checkFile(file, cb){
 const upload = multer({
   storage: storage,
   fileFilter: function(req, file, cb){
-    checkFile(req.file, cb)
+    checkFile(file, cb)
   }
 });
 
